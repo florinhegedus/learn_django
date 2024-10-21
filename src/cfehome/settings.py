27fb9b99a5e554d2cd3f4cc33a9070bf4560444b
florinhegedus <florinxhgds@gmail.com>
2024-10-21 19:02:00 +0300

    update settings.py to include STATICFILES_DIRS

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -142,6 +142,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_BASE_DIR = BASE_DIR / "staticfiles"
+STATICFILES_VENDOR_DIR = STATICFILES_BASE_DIR / "vendors"
+
+
+# source(s) for python manage.py collectstatic
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
