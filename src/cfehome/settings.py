5e3addf300696ad23ed08bff54c941457be55650
florinhegedus <florinxhgds@gmail.com>
2024-10-21 13:38:14 +0300

    add DEBUG environment variable

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)zj3-g88(x0#4lkngy!65x89%so)kp5(m5yep^5he6yj7v_4a$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get("DEBUG")).lower() == "true"
+
+print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app"  # https://saas.prod.railway.app
