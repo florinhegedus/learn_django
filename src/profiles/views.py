994e41873a60995b5f3122876706b49672c7b032
florinhegedus <florinxhgds@gmail.com>
2024-10-26 12:37:44 +0300

    permissions to view users

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -5,10 +5,23 @@ from django.http import HttpResponse
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
+
 @login_required
-def profile_view(request, username=None, *args, **kwargs):
+def profile_list_view(request):
+    context = {
+        "object_list": User.objects.filter(is_active=True)
+    }
+    return render(request, "profiles/list.html", context)
+
+
+@login_required
+def profile_detail_view(request, username=None, *args, **kwargs):
     user = request.user
-    # profile_user_obj = User.objects.get(username=username)
     profile_user_obj = get_object_or_404(User, username=username)
     is_me = profile_user_obj == user
-    return HttpResponse(f"Hello there {username} - {profile_user_obj.id} - {user.id} - {is_me}")
+    context = {
+        "object": profile_user_obj,
+        "instance": profile_user_obj,
+        "owner": is_me
+    }
+    return render(request, "profiles/detail.html", context)
