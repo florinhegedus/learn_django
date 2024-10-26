f89d28b9a3753ff49c416359cf284c338641f082
florinhegedus <florinxhgds@gmail.com>
2024-10-26 15:10:04 +0300

    sync_subs added

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import Group, Permission
 
-# Create your models here.
-class Subscriptions(models.Model):
-    name = models.CharField(max_length=120)
-
-    class Meta:
-        permissions = [
+SUBSCRIPTION_PERMISSIONS = [
             ("advanced", "Advanced Perm"),
             ("pro", "Pro Perm"),
             ("basic", "Basic Perm"),
             ("basic_ai", "Basic AI Perm")
         ]
+
+# Create your models here.
+class Subscription(models.Model):
+    name = models.CharField(max_length=120)
+    active = models.BooleanField(default=True)
+    groups = models.ManyToManyField(Group)
+    permissions = models.ManyToManyField(Permission, limit_choices_to={"content_type__app_label": "subscriptions", "codename__in": [x[0] for x in SUBSCRIPTION_PERMISSIONS]})
+
+    class Meta:
+        permissions = SUBSCRIPTION_PERMISSIONS
