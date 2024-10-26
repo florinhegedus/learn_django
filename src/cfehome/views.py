dec0210dad0c6c73ba58597fd290031791d30a5b
florinhegedus <florinxhgds@gmail.com>
2024-10-26 09:14:35 +0300

    add staff-only page

--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
+from django.conf import settings
 
 from visits.models import PageVisit
+
+
+LOGIN_URL = settings.LOGIN_URL
 
 
 def home_view(request, *args, **kwargs):
@@ -48,4 +53,9 @@ def pw_protected_view(request, *args, **kwargs):
 
 @login_required
 def user_only_view(request, *args, **kwargs):
+    return render(request, "protected/user-only.html", {})
+
+
+@staff_member_required()
+def staff_only_view(request, *args, **kwargs):
     return render(request, "protected/user-only.html", {})
